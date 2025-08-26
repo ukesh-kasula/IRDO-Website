f8b794e57553dd050a956d88d2880811126097e8
Ukeshnani <ukeshnani10@gmail.com>
2025-08-26 23:13:21 +0530

    bugs clear 2

--- src/app/home/payment-page/payment-page.component.ts
+++ src/app/home/payment-page/payment-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-payment-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './payment-page.component.html',
   styleUrl: './payment-page.component.css'
 })
